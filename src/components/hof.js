6cf4915652ae868bae98ad830ef5a042575f569c
sdpathak24 <202001258@daiict.ac.in>
2024-07-09 17:46:59 +0530

    hof updated

--- src/components/hof.js
+++ src/components/hof.js
@@ -2,6 +2,9 @@ import React from "react";
 import "../styles/hof.css";
 import bu_cert from "../imgs/bu_cert.png";
 import basf from "../imgs/basf.png";
+import who from "../imgs/who.png";
+import nasa from "../imgs/nasa.png";
+import uu from "../imgs/uu.png";
 
 const hof = () => {
   return (
@@ -9,12 +12,24 @@ const hof = () => {
       <h2>Hall of Fames</h2>
       <div className="orgs">
         <div className="org">
+          <img src={nasa} alt="" />
+          <h4>NASA</h4>
+        </div>
+        <div className="org">
+          <img src={who} alt="" />
+          <h4>World Health Organization</h4>
+        </div>
+        <div className="org">
           <img src={bu_cert} alt="" />
           <h4>BU-CERT</h4>
         </div>
         <div className="org">
           <img src={basf} alt="" />
           <h4>basf</h4>
+        </div>
+        <div className="org">
+          <img src={uu} alt="" />
+          <h4>Utrecht University</h4>
         </div>
       </div>
     </div>
